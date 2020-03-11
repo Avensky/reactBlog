e7bb18eae8a32d7a8f92d9976cd9617ab90f9aae
uriel zacarias <urielzacarias@gmail.com>
2020-03-11 00:35:07 -0700

    restructured and added post component in prep for page routingf

--- containers/Blog/Blog.js
+++ containers/Blog/Blog.js
@@ -1,58 +1,12 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-
-    state = {
-        posts: [],
-        clickedPost: null,
-        error: false
-    }
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                //console.log(response)
-            })
-            .catch(error => {
-                //console.log(error)
-                this.setState({error: true})
-            })
-    }
-
-    postClickedHandler = (id) => {
-        this.setState({clickedPostId: id})
-    }
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postClickedHandler(post.id)}
-                    />
-            })
-
-        }
         return (
             <div className="Blog">
-                <div>
+                <header>
                     <nav>
                         <ul>
                             <li><a href="/">Home</a></li>
@@ -60,17 +14,8 @@ class Blog extends Component {
                         </ul>
 
                     </nav>
-                </div>
-
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.clickedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                </header>
+                <Posts />
             </div>
         );
     }
