fac43e1ad6e89e241d715ac9461c942e4aff6924
uriel zacarias <urielzacarias@gmail.com>
2020-03-14 13:31:42 -0700

    Added Layout component

---
+++ components/Layout/Layout.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+import classes from './Layout.css';
+import Navbar from '../Navigation/Navbar/Navbar';
+import Sidebar from '../Navigation/Sidebar/Sidebar';
+
+class Layout extends Component {
+    state = {
+        showSidebar: false
+    }
+
+    sidebarClosedHandler = () => {
+        this.setState({showSidebar: false})
+    }
+// best practice to set state in a clean way when it depends on a previous state
+    sidebarToogleHandler = () => {
+        this.setState(( prevState ) => {
+            return {showSidebar: !prevState.showSidebar};
+        });
+    }
+
+    render () {
+        return (    
+            <Auxiliary>
+                <Navbar barToggleClicked={this.sidebarToogleHandler} />
+                <Sidebar open={this.state.showSidebar} closed={this.sidebarClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        )
+    }
+}
+
+export default Layout;
