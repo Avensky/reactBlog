229cb9d2bcc22cef8b3e72a6e37e3c103f370849
uriel zacarias <urielzacarias@gmail.com>
2020-03-14 13:38:16 -0700

    added navbar component

---
+++ components/Navigation/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css'
+import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
+import SidebarToggle from '../Sidebar/SidebarToggle/SidebarToggle';
+
+const navbar = (props) => (
+    <header className={classes.Navbar}>
+        <SidebarToggle clicked={props.sidebarToggleClicked}/>
+        <Logo height="95%" />
+        <nav className={classes.DesktopOnly}>
+            <NavItems />
+        </nav>
+    </header>
+);
+
+export default navbar;
