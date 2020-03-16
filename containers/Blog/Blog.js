81bc4d450bf063d4650c2225a21a75b37864c80c
uriel zacarias <urielzacarias@gmail.com>
2020-03-15 21:24:21 -0700

    updated css tyles for navbar

--- containers/Blog/Blog.js
+++ containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch, } from 'react-router-dom'
 import './Blog.css';
 import Posts from './Posts/Posts';
 //import NewPost from './NewPost/NewPost';
