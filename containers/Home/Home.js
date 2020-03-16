7d7aa3a89f649127ae0a657410f8b82d30ebefb3
uriel zacarias <urielzacarias@gmail.com>
2020-03-16 00:25:59 -0700

    added content to Home page

--- containers/Home/Home.js
+++ containers/Home/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
 import './Home.module.css';
 import Auxiliary from '../../hoc/Auxiliary';
 
 const home = () => {
     return (
         <Auxiliary>
-            <div className="VideoWrapper">
+            <div className="Home">
                 <h1>Hello, I'm <span className="highlight">Uriel Zacarias</span></h1>
                 <h2>full stack web developer</h2>
             </div>
