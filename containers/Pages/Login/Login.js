c45cba7b5bdaf2ee71d054a50b507e4b41e66208
uriel zacarias <urielzacarias@gmail.com>
2020-03-18 00:48:05 -0700

    login page added

--- containers/Pages/Login/Login.js
+++ containers/Pages/Login/Login.js
@@ -7,6 +7,21 @@ const login = () => {
     return(
     <Layout grid="new">
         <Header />
+        <div className="Login">
+                <h1>Log in!</h1>
+                <label>Email:</label>
+                <input type="text" value="Enter Email" />
+                <label>Password:</label>
+                <input type="text" value="Enter Password" />
+                <label>Confirm Password:</label>
+                <input type="text" value="Confirm Password" />
+                <input type="checkbox" className="float" /> <p className="float">Rembember Me</p>
+                <button className="btn" onClick={this.postDataHandler}>Add Post</button>
+                <p><a href="#">Forgot Password</a>.</p>
+                <div class="border-top pt-3"  />
+                <small className="text-muted">Need An Account? <a className="ml-2" href="#">Sign Up Now</a></small>
+                <div class="border-top pt-3"  />
+            </div>
     </Layout>
     )
 }
