7aee935303621152a2300c17dc8c36af2361a83c
uriel zacarias <urielzacarias@gmail.com>
2020-03-27 10:40:42 -0700

    added utility file and updated login form

--- containers/Pages/Login/Login.js
+++ containers/Pages/Login/Login.js
@@ -3,25 +3,51 @@ import Layout from '../../Layout/Layout';
 import Header from '../../Layout/Header/Header';
 import './Login.module.css';
 import '../Pages.module.css';
-
+import { checkValidity } from '../../../utility/utility'
 
 class Login extends Component {
+    state = {
+    }
+
+    login = ( event ) => {
+        event.preventDefault();
+        this.props.onAuth( this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup );
+    }
+
+    inputChangeHandler = ( event ) => {
+        console.log(event.target.value);
+    }
     render () {
         return(
             <Layout grid="one">
-                <div className="Pages">
-                        <legend>Log in!</legend>
-                        <label>Email:</label>
-                        <input type="text" defaultValue="Enter Email" />
-                        <label>Password:</label>
-                        <input type="text" defaultValue="Enter Password" />
-                        <input type="checkbox"/> <p className="inline">Rembember Me</p>
-                        <button className="btn" onClick={this.postDataHandler}>Add Post</button>
-                        <p><a href="#">Forgot Password?</a></p>
-                        <div className="border-top pt-3"  />
-                        <p className="text-muted">Need An Account? <a href="#">Sign Up Now</a></p>
-                        <div className="border-top pt-3"  />
-                    </div>
+                <form className="Pages" method="post" onSubmit={this.login}>
+                    <legend>Log in!</legend>
+                    <label>Email:</label>
+                    <input 
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={this.handleChange}
+                        value={this.state.email}
+                        placeholder="Enter Email"
+                    />
+
+                    <label>Password:</label>
+                    <input 
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={this.handleChange}
+                        value={this.state.password}
+                        placeholder="Enter Password"
+                    />
+                    <input type="checkbox"/> <p className="inline">Rembember Me</p>
+                    <button className="btn">Login</button>
+                    <p><a href="#">Forgot Password?</a></p>
+                    <div className="border-top pt-3"  />
+                    <p className="text-muted">Need An Account? <a href="#">Sign Up Now</a></p>
+                    <div className="border-top pt-3"  />
+                </form>
             </Layout>
             )
     }
