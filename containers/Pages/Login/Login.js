1be1599830f0fe735a88f0f32da48e7c0075b472
uriel zacarias <urielzacarias@gmail.com>
2020-03-27 00:47:51 -0700

    Login is now a smart component

--- containers/Pages/Login/Login.js
+++ containers/Pages/Login/Login.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from '../../Layout/Layout';
 import Header from '../../Layout/Header/Header';
 import './Login.module.css';
 import '../Pages.module.css';
 
 
-const login = () => {
-    return(
-    <Layout grid="one">
-        <div className="Pages">
-                <legend>Log in!</legend>
-                <label>Email:</label>
-                <input type="text" defaultValue="Enter Email" />
-                <label>Password:</label>
-                <input type="text" defaultValue="Enter Password" />
-                <input type="checkbox"/> <p className="inline">Rembember Me</p>
-                <button className="btn" onClick={this.postDataHandler}>Add Post</button>
-                <p><a href="#">Forgot Password?</a></p>
-                <div className="border-top pt-3"  />
-                <p className="text-muted">Need An Account? <a href="#">Sign Up Now</a></p>
-                <div className="border-top pt-3"  />
-            </div>
-    </Layout>
-    )
+class Login extends Component {
+    render () {
+        return(
+            <Layout grid="one">
+                <div className="Pages">
+                        <legend>Log in!</legend>
+                        <label>Email:</label>
+                        <input type="text" defaultValue="Enter Email" />
+                        <label>Password:</label>
+                        <input type="text" defaultValue="Enter Password" />
+                        <input type="checkbox"/> <p className="inline">Rembember Me</p>
+                        <button className="btn" onClick={this.postDataHandler}>Add Post</button>
+                        <p><a href="#">Forgot Password?</a></p>
+                        <div className="border-top pt-3"  />
+                        <p className="text-muted">Need An Account? <a href="#">Sign Up Now</a></p>
+                        <div className="border-top pt-3"  />
+                    </div>
+            </Layout>
+            )
+    }
 }
-export default login;
+export default Login;
