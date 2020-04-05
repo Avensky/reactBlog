66cef2b51dd82a7ba853a34d5d68b6eec89fa1b3
uriel zacarias <urielzacarias@gmail.com>
2020-04-05 02:39:26 -0700

    setting up blog card grid styles

--- src/containers/Pages/Blog/Posts/Post/Post.js
+++ src/containers/Pages/Blog/Posts/Post/Post.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import user from '../../../../../assets/images/user.jpg'
 import classes from './Post.module.css';
+import TextTruncate from 'react-text-truncate'; // recommend
 
 const post = (props) => {
     let assignedClasses = [classes.Post, classes.Card, props.clName]
     return (
         <article className={assignedClasses.join(' ')} onClick={props.clicked}>
             <div className={classes.CardTitle}><h1>{props.title}</h1></div>
-            <div className={classes.CardAuthor}><h2>{props.author}</h2></div> 
-            <div className={classes.CardDescription}><h3>{props.content}</h3></div>
+            <div className={classes.CardDetails}><h2>By{props.author}</h2> <muted>on 2019-12-07</muted></div> 
+            <div className={classes.CardDescription}>
+            <TextTruncate
+                line={4}
+                element="span"
+                truncateText="…"
+                text={props.content}
+                //textTruncateChild={<a href="#">Read on</a>}
+            />
+            </div>
             <figure className={classes.CardThumbnail}>
                 <img src={user} alt="user"/>
             </figure>
