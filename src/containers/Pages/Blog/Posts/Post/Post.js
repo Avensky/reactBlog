eeacced2fa251fe07db79c616f374d0a6d62c233
uriel zacarias <urielzacarias@gmail.com>
2020-04-13 02:14:20 -0700

    hard coded height

--- src/containers/Pages/Blog/Posts/Post/Post.js
+++ src/containers/Pages/Blog/Posts/Post/Post.js
@@ -6,20 +6,19 @@ import classes from './Post.module.css';
 
 const post = (props) => (
 
-    <div className={[
+    <article className={[
         classes.Post,
         classes.Card,
         props.clName
       ].join(' ')} onClick={props.clicked}>
         <div className={classes.CardTitle}><h1>{props.title}</h1></div>
         <div className={classes.CardDetails}><h2>By {props.author}</h2> <p>on 2019-12-07</p></div> 
-        <div className={classes.CardDescription}>
-        {props.content}
-        </div>
+        <div className={classes.CardDescription}></div>
+        <div className={classes.CardDescription}>{props.content}</div>
         <figure className={classes.CardThumbnail}>
             <img src={user} alt="user"/>
         </figure> 
-    </div>
+    </article>
     )
 
 
